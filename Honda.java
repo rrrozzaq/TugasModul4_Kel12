0d38ceb1ceb5ec5bed8c870bd36f7813162592cc
ardhiansaa <94305170+ardhiansaa@users.noreply.github.com>
2022-03-30 22:21:37 +0700

    Update Honda.java

--- Honda.java
+++ Honda.java
@@ -9,4 +9,13 @@ import java.util.Scanner;
             System.out.println("---------------------------------");
             Budget = scan.nextInt();
             System.out.println("Budget anda" + Budget + "juta");
-
+            if (Budget <= 15 ){
+            System.out.println("Budget anda cocok dengan Honda Beat");
+            }
+            else if (Budget >15 && Budget<=25){
+            System.out.println("Budget anda cocok dengan Honda Vario");}
+            else if (Budget > 25){
+            System.out.println("Budget anda cocok dengan Honda PCX");
+            }
+    }
+}
